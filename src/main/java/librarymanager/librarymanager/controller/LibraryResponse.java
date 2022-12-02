27b6bbb22fedb353bea939253a100bcf6d8cc604
Nic <101201789+nic5694@users.noreply.github.com>
2022-12-02 15:36:08 -0500

    initialized the rest controller

---
+++ src/main/java/librarymanager/librarymanager/controller/LibraryResponse.java
@@ -0,0 +1,9 @@
+package librarymanager.librarymanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/libraries")
+public class LibraryResponse {
+}
